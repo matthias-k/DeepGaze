b893ff458d0e3c7d3c9161d8f218d8ff151dab3e
Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>
2021-06-07 23:08:39 +0200

    DeepGazeI weights, loading on CPU
    Signed-off-by: Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>

--- deepgaze_pytorch/deepgaze1.py
+++ deepgaze_pytorch/deepgaze1.py
@@ -12,6 +12,14 @@ from .modules import FeatureExtractor, Finalizer, DeepGazeII as TorchDeepGazeII
 class DeepGazeI(TorchDeepGazeII):
     """DeepGaze I model
 
+    Please note that this version of DeepGaze I is not exactly the one from the original paper.
+    The original model used caffe for AlexNet and theano for the linear readout and was trained using the SFO optimizer.
+    Here, we use the torch implementation of AlexNet (without any adaptations), which doesn't use the two-steam architecture,
+    and the DeepGaze II torch implementation with a simple linear readout network.
+    The model has been retrained with Adam, but still on the same dataset (all images of MIT1003 which are of size 1024x768).
+    Also, we don't use the sparsity penalty anymore.
+
+    Reference:
     Kümmerer, M., Theis, L., & Bethge, M. (2015). Deep Gaze I: Boosting Saliency Prediction with Feature Maps Trained on ImageNet. ICLR Workshop Track. http://arxiv.org/abs/1411.1045
     """
     def __init__(self, pretrained=True):
@@ -31,4 +39,4 @@ class DeepGazeI(TorchDeepGazeII):
         )
 
         if pretrained:
-            raise NotImplementedError()
+            self.load_state_dict(model_zoo.load_url('https://github.com/matthias-k/DeepGaze/releases/download/v1.01/deepgaze1.pth', map_location=torch.device('cpu')))
