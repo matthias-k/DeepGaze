2241662670405d49c00531ce95f996878ad0c94b
Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>
2022-05-09 11:52:51 +0200

    workaround for https://github.com/pytorch/pytorch/issues/74368
    Signed-off-by: Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>

--- deepgaze_pytorch/features/densenet.py
+++ deepgaze_pytorch/features/densenet.py
@@ -23,7 +23,7 @@ class Normalizer(nn.Module):
 class RGBDenseNet201(nn.Sequential):
     def __init__(self):
         super(RGBDenseNet201, self).__init__()
-        self.densenet = torch.hub.load('pytorch/vision:v0.5.0', 'densenet201', pretrained=True) 
+        self.densenet = torch.hub.load('pytorch/vision:v0.6.0', 'densenet201', pretrained=True) 
         self.normalizer = Normalizer()
         super(RGBDenseNet201, self).__init__(self.normalizer, self.densenet)
 
